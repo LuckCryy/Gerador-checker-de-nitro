6e9edfc72b0d1c631224827f1e0f9ebd5ec6f77f
Luckzin66 <82342585+Luckzin66@users.noreply.github.com>
2021-05-13 22:57:13 -0300

    Add files via upload

---
+++ gerador nitro/Gerador.py
@@ -0,0 +1,69 @@
+import random, string
+import webbrowser
+import time
+import requests
+from colorama import Fore, init
+init()
+
+
+
+print(Fore.BLUE+ """
+███╗░░██╗██╗████████╗██████╗░░█████╗░░░░░░░░██████╗░██╗███████╗████████╗
+████╗░██║██║╚══██╔══╝██╔══██╗██╔══██╗░░░░░░██╔════╝░██║██╔════╝╚══██╔══╝
+██╔██╗██║██║░░░██║░░░██████╔╝██║░░██║█████╗██║░░██╗░██║█████╗░░░░░██║░░░
+██║╚████║██║░░░██║░░░██╔══██╗██║░░██║╚════╝██║░░╚██╗██║██╔══╝░░░░░██║░░░
+██║░╚███║██║░░░██║░░░██║░░██║╚█████╔╝░░░░░░╚██████╔╝██║██║░░░░░░░░██║░░░
+╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░░░░░░░╚═════╝░╚═╝╚═╝░░░░░░░░╚═╝░░░
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+░░░░░░░░█████╗░██╗░░██╗███████╗░█████╗░██╗░░██╗███████╗██████╗░
+░░██╗░░██╔══██╗██║░░██║██╔════╝██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+██████╗██║░░╚═╝███████║█████╗░░██║░░╚═╝█████═╝░█████╗░░██████╔╝
+╚═██╔═╝██║░░██╗██╔══██║██╔══╝░░██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
+░░╚═╝░░╚█████╔╝██║░░██║███████╗╚█████╔╝██║░╚██╗███████╗██║░░██║
+░░░░░░░░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝""")
+time.sleep(1.5)
+print(Fore.LIGHTCYAN_EX + "Criador  - lucas_asz ")
+time.sleep(0.3)
+print("Discord: !  Lucas_asz#5555 \n")
+time.sleep(0.2)
+
+num = input('Digite o número de codigos que você quer que o programa gere e cheque:')
+
+f = open("Nitro Codes.txt", "w", encoding='utf-8')
+
+print("Aguarde, estamos gerando!")
+time.sleep(0.5)
+
+for n in range(int(num)):
+    y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+    f.write('https://discord.gift/')
+    f.write(y)
+    f.write("\n")
+
+f.close()
+
+# =============Checker=========================
+log = open('Valids.txt', 'w')
+
+with open("Nitro Codes.txt") as f:
+    for line in f:
+        nitro = line.strip("\n")
+
+        url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            print(Fore.GREEN + " Valid | {}".format(line.strip("\n")))
+            log.write(" Valid | {}\n ".format(line.strip("\n")))
+log.close()
+
+time.sleep(0.5)
+print(Fore.RESET + 'Copyright©2021, lucas_asz. Todos os direitos reservados.')
+time.sleep(0.3)
+input('Aperte enter para fechar o programa!')
---
+++ gerador nitro/Readme.txt
@@ -0,0 +1,2 @@
+O programa irá gerar código de nitro e verifica-los automaticamente
+ só irá aparecer na tela os codigo válidos e ele irá criar um .txt com todos os códigos válidos.
